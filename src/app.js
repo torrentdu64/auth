01a020ce58a469accd21ed20dbf800fa647d00b6
torrent benoit <torrentdu64@gmail.com>
2017-09-10 12:08:49 +0200

    create firebase compte and config to app connect

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
 import {Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBGltpKYhHXsRNFTHM_I8Zl1ON34oYS3ZI",
+      authDomain: "auth-4ce11.firebaseapp.com",
+      databaseURL: "https://auth-4ce11.firebaseio.com",
+      projectId: "auth-4ce11",
+      storageBucket: "auth-4ce11.appspot.com",
+      messagingSenderId: "803019534852"
+    });
+  }
   render(){
     return (
       <View>
