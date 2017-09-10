7603369384bb02286e42a858a8eb2bb982f5bae4
torrent benoit <torrentdu64@gmail.com>
2017-09-10 15:39:40 +0200

    add form to state email and pwd

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
-import {Card, CardSection, Button } from './common';
+import {Card, CardSection, Button, Input  } from './common';
 
 class LoginForm extends Component {
+
+  state = { email: '', password: '' };
+
   render() {
     return (
       <Card >
-        <CardSection />
-        <CardSection />
+        <CardSection >
+          <Input
+          placeholder="toto@gmail.com"
+          label="E-mail"
+          value={this.state.email}
+          onChangeText={ email => this.setState({ email })}
+
+          />
+        </CardSection>
+
+        <CardSection >
+          <Input
+          placeholder="*********"
+          label="Password"
+          value={this.state.password}
+          onChangeText={ password => this.setState({ password })}
+          secureTextEntry
+          />
+        </CardSection>
 
         <CardSection>
           <Button>
