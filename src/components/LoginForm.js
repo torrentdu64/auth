f7f9b6221b37485f8f3ece9ecabbf7c8e4f9eb8d
torrent benoit <torrentdu64@gmail.com>
2017-09-10 17:13:06 +0200

    step auth error create user and sign in

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,42 @@
 import React, {Component} from 'react';
-import {Card, CardSection, Button, Input  } from './common';
+import {Text} from 'react-native';
+import firebase from 'firebase';
+import {Card, CardSection, Button, Input, Spinner  } from './common';
 
 class LoginForm extends Component {
 
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '', loading: false };
+
+
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '', loading: true });
+
+     firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch( () => {
+          firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch( () => {
+              this.setState({ error: 'Une erreur est survenue :( ' });
+          });
+      });
+    }
+
+    renderButton(){
+      if(this.state.loading){
+        return <Spinner size="small" />;
+      }
+      return (
+        <Button onPress={this.onButtonPress.bind(this)}>
+            Log in
+          </Button>
+        );
+    }
+
 
   render() {
+     const { errorStyle } = styles;
     return (
       <Card >
         <CardSection >
@@ -14,7 +45,6 @@ class LoginForm extends Component {
           label="E-mail"
           value={this.state.email}
           onChangeText={ email => this.setState({ email })}
-
           />
         </CardSection>
 
@@ -28,15 +58,26 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={errorStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
-            Log in
-          </Button>
+              {this.renderButton()}
         </CardSection>
       </Card >
 
       );
   }
 }
+
+const styles = {
+  errorStyle: {
+    color: 'red',
+    fontSize: 20,
+    alignSelf: 'center'
+  }
+
+};
 
 export default LoginForm;
