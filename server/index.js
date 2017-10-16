e62720704d24124e9f4b4869d35d16b36b9a84f7
torrent benoit <torrentdu64@gmail.com>
2017-10-16 15:06:22 +0200

    create a server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const http =require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+
+
+
+
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('my port', port);
