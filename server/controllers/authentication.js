0420dbaf7f0cb885b6b89e4736a4212ceb8d5d27
torrent benoit <torrentdu64@gmail.com>
2017-10-17 12:02:07 +0200

    save user to database and err msg

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -6,5 +6,24 @@ exports.signup = function(req,res, next){
 
    User.findOne({email: email}, function(err, existingUser){
 
+        if(err){return next(err);}
+
+        if(existingUser){
+          return res.status(422).send({error: 'Email is already existing'});
+        }
+
+        const user = new User({
+          email: email,
+          password: password
+        });
+
+        user.save(function(err){
+
+          if(err){
+            return next(err);
+          }
+
+        res.json({ success: true });
+        })
    });
-};
+}
