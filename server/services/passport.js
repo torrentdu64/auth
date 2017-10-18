e6267a9d9270972a4227fc10ef962a00006ad8c6
torrent benoit <torrentdu64@gmail.com>
2017-10-18 15:02:40 +0200

    sign in with local strategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,7 +3,13 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
+
+const localOptions = { usernameField: 'email'};
+const localLogin = new LocalStrategy( localOptions, function(email, password, done){
+
+});
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
